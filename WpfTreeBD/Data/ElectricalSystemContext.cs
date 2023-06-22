bcfba6dba69d536d4b842e14d0804adf1dc3e7bc
ShadowGreg <shadow-greg@yandex.ru>
2023-06-22 15:03:54 +0300

    добавил работу с БД

---
+++ WpfTreeBD/Data/ElectricalSystemContext.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.Entity;
+using System.Data.SQLite;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using WpfTreeBD.Core;
+
+namespace WpfTreeBD.Data {
+    public class ElectricalSystemContext : DbContext {
+        public ElectricalSystemContext() : base(new SQLiteConnection()
+        {
+            ConnectionString = new SQLiteConnectionStringBuilder()
+            {
+                DataSource = "ElectricalSystem.db",
+                ForeignKeys = true
+            }.ConnectionString
+        }, true)
+        {
+        }
+        public DbSet<ElectricalSystem> ElectricalSystems { get; set; }
+        public DbSet<ElectricPanel> ElectricPanels { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ WpfTreeBD/Data/ElectricalSystemRepository.cs
@@ -0,0 +1,52 @@
+﻿using WpfTreeBD.Core;
+
+namespace WpfTreeBD.Data {
+    public class ElectricalSystemRepository {
+        public void AddOrUpdateElectricalSystem(ElectricalSystem electricalSystem)
+        {
+            using (var context = new ElectricalSystemContext())
+            {
+                var existingElectricalSystem = context.ElectricalSystems.Find(electricalSystem.SelfId);
+                if (existingElectricalSystem == null)
+                {
+                    context.ElectricalSystems.Add(electricalSystem);
+                }
+                else
+                {
+                    context.Entry(existingElectricalSystem).CurrentValues.SetValues(electricalSystem);
+                }
+                context.SaveChanges();
+            }
+        }
+        public ElectricalSystem GetElectricalSystemById(string id)
+        {
+            using (var context = new ElectricalSystemContext())
+            {
+                return context.ElectricalSystems.Find(id);
+            }
+        }
+        public void AddOrUpdateElectricPanel(ElectricPanel electricPanel)
+        {
+            using (var context = new ElectricalSystemContext())
+            {
+                var existingElectricPanel = context.ElectricPanels.Find(electricPanel.SelfId);
+                if (existingElectricPanel == null)
+                {
+                    context.ElectricPanels.Add(electricPanel);
+                }
+                else
+                {
+                    context.Entry(existingElectricPanel).CurrentValues.SetValues(electricPanel);
+                }
+                context.SaveChanges();
+            }
+        }
+        public ElectricPanel GetElectricPanelById(string id)
+        {
+            using (var context = new ElectricalSystemContext())
+            {
+                return context.ElectricPanels.Find(id);
+            }
+        }
+    }
+}
